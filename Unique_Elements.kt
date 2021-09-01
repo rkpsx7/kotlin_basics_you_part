88a5e1eca440bd4992f117eeafc63318114b1c75
Akash Kumar Prajapat <38742274+rkpsx7@users.noreply.github.com>
2021-09-01 20:22:08 +0530

    Add files via upload

--- Unique_Elements.kt
+++ Unique_Elements.kt
@@ -1,8 +1,16 @@
 import java.util.*
 
-fun main(){
-    var sc = Scanner(System.`in`)
-    var arr = intArrayOf()
-
-
+fun main() {
+    val sc = Scanner(System.`in`)
+    var str = sc.nextLine()
+    val hashmap = LinkedHashMap<String, Int>()
+    val strArr = str.split(" ".toRegex()).toTypedArray()
+    for (ch in strArr) {
+        if (hashmap.containsKey(ch)) {
+            hashmap[ch] = hashmap[ch]!! + 1
+        } else hashmap[ch] = 1
+    }
+    for ((key, value) in hashmap) {
+        if (value == 1) print("$key ")
+    }
 }
